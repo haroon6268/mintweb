f9b239591529a0085289f98cfdb2728c98ab9c0f
haroon6268 <136369742+haroon6268@users.noreply.github.com>
2025-05-10 13:01:42 -0400

    Fixed spacing

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-"use client";
 import Image from "next/image";
 import Navbar from "./ components/Navbar";
 import Hero from "./ components/Hero";
@@ -7,17 +6,8 @@ import Testimonials from "./ components/Testimonials";
 import LoveLanguage from "./ components/LoveLanguage";
 import Pricing from "./ components/Pricing";
 import { Stats } from "./ components/Stats";
-import { useEffect } from "react";
+
 export default function Home() {
-	useEffect(() => {
-		console.log("test");
-		var docWidth = document.documentElement.offsetWidth;
-		[].forEach.call(document.querySelectorAll("*"), function (el) {
-			if (el.offsetWidth > docWidth) {
-				console.log(el);
-			}
-		});
-	}, []);
 	return (
 		<div>
 			<Navbar />
